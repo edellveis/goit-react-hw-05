742edecd45f3835bb75d8a58d625f9e805088150
edellveis <161501144+edellveis@users.noreply.github.com>
2024-10-20 19:51:16 +0300

    23

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 export default function App() {
-  return <div>App</div>;
+  return <div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+  </div>;
 }
